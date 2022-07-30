3864db42a790390c946e9ec560f3a2384b7ca1ba
Pal Makkar <pmakkar97@gmail.com>
2022-07-29 17:56:39 -0700

    Initial Commit

---
+++ PickUp Driver/PickUp_DriverApp.swift
@@ -0,0 +1,17 @@
+//
+//  PickUp_DriverApp.swift
+//  PickUp Driver
+//
+//  Created by Pal Makkar on 7/29/22.
+//
+
+import SwiftUI
+
+@main
+struct PickUp_DriverApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
